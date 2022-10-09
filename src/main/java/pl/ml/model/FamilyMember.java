d996a9dc6418672dc95632577ec5989d19fd6099
Michał Lemański <michael51@tlen.pl>
2022-10-09 11:32:32 +0200

    Dodanie testów

--- src/main/java/pl/ml/model/FamilyMember.java
+++ src/main/java/pl/ml/model/FamilyMember.java
@@ -1,10 +1,17 @@
 package pl.ml.model;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+@Setter
+@Getter
+@NoArgsConstructor
 @Entity
 public class FamilyMember {
     @Id
@@ -15,53 +22,4 @@ public class FamilyMember {
     private Integer age;
     private Long familyId;
 
-    public FamilyMember() {
-    }
-
-    public FamilyMember(String familyName, String givenName, Integer age, Long familyId) {
-        this.familyName = familyName;
-        this.givenName = givenName;
-        this.age = age;
-        this.familyId = familyId;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getFamilyName() {
-        return familyName;
-    }
-
-    public void setFamilyName(String familyName) {
-        this.familyName = familyName;
-    }
-
-    public String getGivenName() {
-        return givenName;
-    }
-
-    public void setGivenName(String givenName) {
-        this.givenName = givenName;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public Long getFamilyId() {
-        return familyId;
-    }
-
-    public void setFamilyId(Long familyId) {
-        this.familyId = familyId;
-    }
 }
