bb7a5a68d613d5dae9a34e5df1be91dcbcb6242d
Michał Lemański <michael51@tlen.pl>
2022-10-09 20:35:19 +0200

    małe poprawki

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,10 +1,10 @@
 INSERT INTO family_member
-    (id, family_name, given_name, age, family_id)
+    (family_name, given_name, age, family_id)
 VALUES
-    (1, 'Kowalski', 'Marcin', 36, 1),
-    (2, 'Kowalska', 'Agata', 33, 1),
-    (3, 'Kowalski', 'Brajan', 2, 1),
-    (4, 'Kowalski', 'Mateusz', 11, 1),
-    (5, 'Nowak', 'Ela', 22, 2),
-    (6,'Nowak', 'Mariusz', 33, 2),
-    (7, 'Nowak', 'Krzysztof', 9, 2);
+    ('Kowalski', 'Marcin', 36, 1),
+    ('Kowalska', 'Agata', 33, 1),
+    ('Kowalski', 'Brajan', 2, 1),
+    ('Kowalski', 'Mateusz', 11, 1),
+    ('Nowak', 'Ela', 22, 2),
+    ('Nowak', 'Mariusz', 33, 2),
+    ('Nowak', 'Krzysztof', 9, 2);
